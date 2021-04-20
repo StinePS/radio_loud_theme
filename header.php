d62c900a354e31f65f3ca2b64eb95e53d73780a5
StinePS <stin039m@stud.kea.dk>
2021-04-20 14:46:15 +0200

    H'er styling

--- header.php
+++ header.php
@@ -13,37 +13,50 @@
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="robots" content=noindex>
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 	<?php wp_head(); ?>
+	<!-- fonts -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500&family=Gugi&display=swap" rel="stylesheet">  
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php _e(
+     'Skip to content',
+     'twentynineteen'
+ ); ?></a>
 
-		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
+		<header id="masthead" class="<?php echo is_singular() &&
+  twentynineteen_can_show_post_thumbnail()
+      ? 'site-header featured-image'
+      : 'site-header'; ?>">
 
 			<div class="site-branding-container">
-				<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
+				<?php get_template_part('template-parts/header/site', 'branding'); ?>
 			</div><!-- .site-branding-container -->
 
-			<?php if ( is_singular() && twentynineteen_can_show_post_thumbnail() ) : ?>
+			<?php if (is_singular() && twentynineteen_can_show_post_thumbnail()): ?>
 				<div class="site-featured-image">
 					<?php
-						twentynineteen_post_thumbnail();
-						the_post();
-						$discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null;
+     twentynineteen_post_thumbnail();
+     the_post();
+     $discussion =
+         !is_page() && twentynineteen_can_show_post_thumbnail()
+             ? twentynineteen_get_discussion_data()
+             : null;
 
-						$classes = 'entry-header';
-					if ( ! empty( $discussion ) && absint( $discussion->responses ) > 0 ) {
-						$classes = 'entry-header has-discussion';
-					}
-					?>
+     $classes = 'entry-header';
+     if (!empty($discussion) && absint($discussion->responses) > 0) {
+         $classes = 'entry-header has-discussion';
+     }
+     ?>
 					<div class="<?php echo $classes; ?>">
-						<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+						<?php get_template_part('template-parts/header/entry', 'header'); ?>
 					</div><!-- .entry-header -->
 					<?php rewind_posts(); ?>
 				</div>

86ed5a570947651b5de62244d0ef42568ea596f8
StinePS <stin039m@stud.kea.dk>
2021-04-22 10:56:11 +0200

    Lidt af hvert...

--- header.php
+++ header.php
@@ -31,36 +31,10 @@
      'twentynineteen'
  ); ?></a>
 
-		<header id="masthead" class="<?php echo is_singular() &&
-  twentynineteen_can_show_post_thumbnail()
-      ? 'site-header featured-image'
-      : 'site-header'; ?>">
-
-			<div class="site-branding-container">
-				<?php get_template_part('template-parts/header/site', 'branding'); ?>
-			</div><!-- .site-branding-container -->
-
-			<?php if (is_singular() && twentynineteen_can_show_post_thumbnail()): ?>
-				<div class="site-featured-image">
-					<?php
-     twentynineteen_post_thumbnail();
-     the_post();
-     $discussion =
-         !is_page() && twentynineteen_can_show_post_thumbnail()
-             ? twentynineteen_get_discussion_data()
-             : null;
-
-     $classes = 'entry-header';
-     if (!empty($discussion) && absint($discussion->responses) > 0) {
-         $classes = 'entry-header has-discussion';
-     }
-     ?>
-					<div class="<?php echo $classes; ?>">
-						<?php get_template_part('template-parts/header/entry', 'header'); ?>
-					</div><!-- .entry-header -->
-					<?php rewind_posts(); ?>
-				</div>
-			<?php endif; ?>
+		<header>
+			<div class="header_container">
+				<div id="loud_logo"><a href="/"><img src="https://stineplejdrup.dk/kea/09_cms/radio_loud/loud_logo.jpg" alt="Radio Loud logo"></a></div>
+				<button id="button_loud_live">LOUD Live</button></div>
 		</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
--- page-programmer.php
+++ page-programmer.php
@@ -9,30 +9,27 @@
  * @since Twenty Nineteen 1.0
  */
 
-get_header();
-?>
+get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<?php
+			<?php // Start the Loop.
+// Start the Loop.
+?>while (have_posts()):
+       the_post();
 
-			// Start the Loop.
-			while ( have_posts() ) :
-				the_post();
+       get_template_part('template-parts/content/content', 'page');
 
-				get_template_part( 'template-parts/content/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
-			endwhile; // End the loop.
-			?>
+       // If comments are open or we have at least one comment, load up the comment template.
+       if (comments_open() || get_comments_number()) {
+           comments_template();
+       }
+   endwhile;
+// End the loop.
+?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
-get_footer();
+<?php get_footer();

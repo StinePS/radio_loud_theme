777ada504d7f5cad1fd38d00192c21a648f46c50
StinePS <stin039m@stud.kea.dk>
2021-04-23 09:39:20 +0200

    ._-filer slettet

--- single-podcast.php
+++ single-podcast.php
@@ -11,61 +11,61 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
-
         
-			<?php
-// Start the Loop.
-// Start the Loop.
-?>while (have_posts()):
-       the_post();
+        <article class="single_view grid-2">
+            <div>
+                <h1 class="single_overskrift"></h1>
+                <p class="podcast_description"></p>
+            </div>
+            <div>
+                <img src="" alt="">
+            </div>
+        </article>
 
-       get_template_part('template-parts/content/content', 'single');
+        <section id="episodes">
+            <template>
+                <article>
+                    <img src="" alt="">
+                    <div>
+                        <h2></h2>
+                        <p class="episode_description"></p>
+                        <a href="">Afspil</a>
+                    </div>
+                </article>
+            </template>
+        </section>
+        
+        </main><!-- #main -->
+        
 
-       if (is_singular('attachment')) {
-           // Parent post navigation.
-           the_post_navigation([
-               /* translators: %s: Parent post link. */
-               'prev_text' => sprintf(
-                   __(
-                       '<span class="meta-nav">Published in</span><span class="post-title">%s</span>',
-                       'twentynineteen'
-                   ),
-                   '%title'
-               ),
-           ]);
-       } elseif (is_singular('post')) {
-           // Previous/next post navigation.
-           the_post_navigation([
-               'next_text' =>
-                   '<span class="meta-nav" aria-hidden="true">' .
-                   __('Next Post', 'twentynineteen') .
-                   '</span> ' .
-                   '<span class="screen-reader-text">' .
-                   __('Next post:', 'twentynineteen') .
-                   '</span> <br/>' .
-                   '<span class="post-title">%title</span>',
-               'prev_text' =>
-                   '<span class="meta-nav" aria-hidden="true">' .
-                   __('Previous Post', 'twentynineteen') .
-                   '</span> ' .
-                   '<span class="screen-reader-text">' .
-                   __('Previous post:', 'twentynineteen') .
-                   '</span> <br/>' .
-                   '<span class="post-title">%title</span>',
-           ]);
-       }
+        <script>
+            let podcast;
+            let episodes;
+            let chosenPodcast = <?php echo get_the_ID(); ?>;
 
-       // If comments are open or we have at least one comment, load up the comment template.
-       if (comments_open() || get_comments_number()) {
-           comments_template();
-       }
-   endwhile;
-// End the loop.
-?>
+            const dbUrl = "https://stineplejdrup.dk/kea/09_cms/radio_loud/wp-json/wp/v2/podcasts/" + chosenPodcast;
+            const episodeUrl = "https://stineplejdrup.dk/kea/09_cms/radio_loud/wp-json/wp/v2/episoder?per_page=100";
+            const container = document.querySelector("#episoder");
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            async function getJson() {
+                const data = await fetch(dbUrl);
+                podcast = await data.json();
+
+                const data2 = await fetch(episodeUrl);
+                episoder = await data2.json();
+                console.log("episoder: ", episoder);
+
+                showPodcasts();
+                showEpisodes();
+            }
+
+            showPodcasts() {
+                console.log("showPodcasts");
+            }
+
+        </script>
+</section><!-- #primary -->
 
 <?php get_footer();

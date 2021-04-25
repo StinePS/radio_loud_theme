7bcab6e8b673cdbf5bc047c18a5356e2c7b83b2b
StinePS <stin039m@stud.kea.dk>
2021-04-25 20:38:09 +0200

    Loud lab-h1

--- page-loud-lab.php
+++ page-loud-lab.php
@@ -14,17 +14,23 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<?php // Start the Loop.
-   while (have_posts()):
-       the_post();
+        <header class="entry-header">
+		
+        <h1 class="entry-title">Loud <span class="red">L</span><span class="pink">a</span><span class="blue">b</span></h1>
+            </header>
 
-       get_template_part('template-parts/content/content', 'page');
+            <?php // Start the Loop.
+// Start the Loop.
+?>while (have_posts()):
+                the_post();
 
-       // If comments are open or we have at least one comment, load up the comment template.
-       if (comments_open() || get_comments_number()) {
-           comments_template();
-       }
-   endwhile;
+                get_template_part('template-parts/content/content', 'page');
+
+                // If comments are open or we have at least one comment, load up the comment template.
+                if (comments_open() || get_comments_number()) {
+                    comments_template();
+                }
+            endwhile;
 // End the loop.
 ?>
 

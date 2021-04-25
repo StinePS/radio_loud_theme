fb0d411743c5aca5b55622e69717768c8cf2b030
StinePS <stin039m@stud.kea.dk>
2021-04-25 21:10:11 +0200

    Forsøg 2 med p-styling

--- page-loud-lab.php
+++ page-loud-lab.php
@@ -19,16 +19,16 @@ get_header(); ?>
         </header>
 
         <h2>Du skaber LOUD</h2>
-        <section class="p_text">
-        <p> LAB er for dig, der gerne vil være med til at forme LOUD. <br>
+        <section>
+        <p class="p_text"> LAB er for dig, der gerne vil være med til at forme LOUD. <br>
             LOUD skal være et medie for alle unge, og derfor er det vigtigt, at alle er repræsenteret i LAB: erhvervsskoleelever, universitetsstuderende, de jobsøgende, de stille, de larmende, de aktive, sofaflyderne.</p>
-        <p> Vi holder LOUD LAB én gang om måneden i hele Danmark, og her kan du 
+        <p class="p_text"> Vi holder LOUD LAB én gang om måneden i hele Danmark, og her kan du 
             sammen med andre unge fra hele landet være med til at udvikle nye podcasts, prøve kræfter med at producere lyd, og du kan pitche dine podcastidéer.</p>
-        <p> Det kræver ingen specielle forudsætninger at være med. Faktisk er 
+        <p class="p_text"> Det kræver ingen specielle forudsætninger at være med. Faktisk er 
             det eneste krav, at du er mellem 15 og 32 år og har lyst til at være med til at udvikle vores podcasts og events.</p>
-        <p> For at komme med på LOUD LAB skal du skrive til 
+        <p class="p_text"> For at komme med på LOUD LAB skal du skrive til 
             mailto:lab@radioloud.dk og kort fortælle, hvem du er, og hvorfor du har lyst til at være med.</p>
-        <p> PÅ GRUND AF CORONARESTRIKTIONERNE KAN VI FOR ØJEBLIKKET IKKE 
+        <p class="p_text"> PÅ GRUND AF CORONARESTRIKTIONERNE KAN VI FOR ØJEBLIKKET IKKE 
             AFHOLDE LOUD LAB. DU KAN DOG STADIG GODT TILMELDE DIG, SÅ DU KAN FÅ INFORMATION OM, HVORNÅR VI IGEN SAMLES FOR AT UDVIKLE LOUD.</p>
         </section>
 		</main><!-- #main -->

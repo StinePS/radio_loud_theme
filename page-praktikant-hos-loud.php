0b4b5ae1238fd752e7be9bdb81fda77a9bead91b
StinePS <stin039m@stud.kea.dk>
2021-04-26 11:39:39 +0200

    Praktikant

---
+++ page-praktikant-hos-loud.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+get_header(); ?>
+
+<div class="overskrift">
+    <h1>Praktikant hos L<span class="red">o</span><span class="pink">u</span><span class="blue">d</span>
+    </h1>
+</div>
+
+    <section>
+        <h2>Praktik</h2>
+        <p>Praktikmesse holdes online onsdag den 21. april kl. 13.00 <br>
+        Åbent Hus i uge 17 (26. april – 2. maj) <br>
+        Store matchdag afvikles torsdag den 6. maj.</p>
+    </section>
+        
+    <section>
+        <h3>Er du journaliststuderende og skal søge praktik?</h3>
+        <p>Dette forår søger vi fem journalistpraktikanter, der er klar på at lære en masse og tage det næste skridt sammen med LOUD. </p>
+
+        <h4>Overskrift?</h4>
+        <p>På LOUD har vi fokus på den konstruktive tilgang til journalistik og fortælling i vores podcasts og programmer. <br> Det betyder, at vi arbejder med at skabe viden, indsigter, perspektiver og løsninger på vores vinkler og historier.</p>
+
+        <p>Journalistisk holder vi især et skarpt øje med kultur, EU, uddannelse, udland, ungeliv, sociale forhold og politik set med yngre øjne, så har du interesse i disse områder er det kun en fordel.</p>
+    </section>
+
+    <section>
+        <h3>LOUD som praktikplads</h3>
+        <p>Vi er en ung radio og et ungt hold, og derfor husker mange af os tydeligt vores egen praktiktid. Vi ved, hvor vigtigt et godt praktikophold er, og alle på redaktionen hjælper med at få det til at ske. Inklusive dig selv.</p>
+        <p>Derfor er der også stor fleksibilitet under praktikken, og der er mulighed for at dele dit ophold op, så du kommer forbi begge redaktioner.
+        Vi søger tre praktikanter til vores nyhedsredaktion i Svendborg og to praktikanter til Aktualitet og Kultur i København.</p>
+    </section>
+
+    <section>
+        <h3>Praktikmuligheder</h3>
+        <p>12 måneders praktik på LOUD Nyheder. <br>
+        8 måneder på̊ Nyhedsredaktionen i Svendborg (eller alle 12 måneder, hvis det er ønsket) <br>
+        4 måneder på valgfri redaktion – efter sparring med praktikantvejleder. <br>
+
+        Du kommer til at arbejde med at finde og skabe nyheder, udvikle podcasts, nyhedsfortælling, tilrettelæggelse, manus, afvikling, vinkling, interview, reportage og konstruktiv journalistik. </p>
+
+        <h3>Ydeligere information</h3>
+        <p>Hvis du vil høre mere om LOUD som praktiksted, er du velkommen til at skrive til <br>
+        Praktikantvejleder (Svendborg) Martin Sodemann: <a href="mailto:martins@radioloud.dk">martins@radioloud.dk</a> <br>
+
+        Praktikantvejleder (København) Camilla Michelle Mikkelsen:  <a href="mailto:camillamichelle@radioloud.dk">camillamichelle@radioloud.dk</a>
+        </p>
+    </section>
+<?php get_footer();
